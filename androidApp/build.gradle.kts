22c2b62fbec4e81dabd83a92bc0f9cbd64631a6f
hit1995 <33278991+hit1995@users.noreply.github.com>
2025-10-22 22:06:43 +0800

    Update build.gradle.kts

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
     defaultConfig {
         applicationId = "com.alpha.showcase"
-        minSdk = libs.versions.android.minSdk.get().toInt()
+        minSdk = 19 #libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = project.extra["versionCode"] as Int
         versionName = project.extra["versionName"] as String
